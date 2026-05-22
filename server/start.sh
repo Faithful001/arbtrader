5825dcc42b29d3bc3225a6f95b7347156d09a942
King Faithful <faithfuleromosele001@gmail.com>
2026-05-22 23:12:50 +0100

    make start.sh executable

--- server/start.sh
+++ server/start.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-cd /opt/render/project/src  # Render's working directory
 celery -A src.infrastructure.celery.app worker --beat --loglevel=info --pool=solo &
 uvicorn main:app --host 0.0.0.0 --port $PORT
